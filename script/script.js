f8563d898c1d9403f6fce45d7cf9d3ae39209f6b
Mirsaid <mirsaidakbarov094@gmail.com>
2023-03-31 17:18:24 +0500

    updated

--- script/script.js
+++ script/script.js
@@ -4,12 +4,6 @@ let selectFilter = document.querySelector("[name='status']");
 
 
 
-
-
-
-
-
-
 const filterByStatus = (status) => {
     switch (status) {
         case "completed":
@@ -42,7 +36,8 @@ const render = () => {
     items.innerHTML = "";
 
 
-    filterByStatus(currentStatus).forEach((v) => {
+    filterByStatus(currentStatus).forEach((v, i) => {
+
 
 
 
@@ -60,40 +55,92 @@ const render = () => {
 
         const editButton = v.isDone
             ? ""
-            : `   <button class="edit">
-            <img src="./images/pencil.svg" alt="">
+            : `   <button class="edit material-symbols-outlined">
+            edit
             </button>`;
 
 
         items.innerHTML += `
-        <li class="item" id="${v.id}">
+        <li  draggable=true class="item" id="${v.id}">
+       
+        <div class="left__button">
+        <span class="order">${i + 1}.</span>
 
           ${checkbox}
           ${input}
+          </div>
 
           <div class="buttons">
 
          ${editButton}
+         
+           <button style="display:none" id="${v.id}" class="cancel  material-symbols-outlined">
+           close
+           </button>
 
-            <button style="display:none" id="${v.id}" class="save">
-            <img src="./images/112.svg" alt="">
+            <button style="display:none" id="${v.id}" class="save  material-symbols-outlined">
+            bookmark
             </button>
 
 
-              <button style="display:none" id="${v.id}" class="cancel">
-              <img src="./images/BxX.svg" alt="">
-              </button>
 
-
-            <button id="${v.id}" class="delete">
-            <img src="./images/trash.svg" alt="">
+            <button id="${v.id}" class="delete  material-symbols-outlined">
+            delete
             </button>
             
           </div>
         </li>
       `;
     });
-};
+
+    const list = document.getElementsByClassName("item");
+
+    for (let item of list) {
+        item.addEventListener("dragstart", (e) => {
+            const currentId = e.target.closest(".item")?.id;
+            dragIndex = todos.findIndex((v) => v.id == currentId);
+            e.target.closest(
+                ".item"
+            ).style.cssText = `opacity:0.5;border:2px solid #3498db;background-color:#f1f1f1;transform: scale(1.05);`;
+
+            console.log("start", currentId);
+        });
+
+        item.addEventListener("dragend", (e) => {
+            e.preventDefault();
+            e.target.closest(".item").style.cssText = `opacity:1;border:1px solid #ccc;background-color: #fff;transform: scale(1);`;
+        });
+
+        item.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            e.target.closest(
+                ".item"
+            ).style.cssText = `border-bottom:2px solid #3498db;transform: scale(1.05);`;
+        });
+
+        item.addEventListener("dragleave", (e) => {
+            console.log("leave");
+            e.target.closest(
+                ".item"
+            ).style.cssText = `border-bottom:1px solid #ccc;transform: scale(1);`;
+        });
+
+        item.addEventListener("drop", (e) => {
+            e.preventDefault();
+            const currentId = e.target.closest(".item")?.id;
+            const dropIndex = todos.findIndex((v) => v.id == currentId);
+
+            let a = todos.splice(dragIndex, 1);
+            todos.splice(dropIndex, 0, a[0]);
+
+            // const temp = todos[dragIndex];
+            // todos[dragIndex] = todos[dropIndex];
+            // todos[dropIndex] = temp;
+
+            render();
+        });
+    }
+}
 
 
 
@@ -112,11 +159,18 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const inputValue = e.target.elements.todo.value;
 
+
     if (inputValue) {
-        const newTodo = { id: "s" + new Date().getTime(), todo: inputValue };
+
+
+        const newTodo = {
+            id: "s" + new Date().getTime(),
+            todo: inputValue
+        };
         todos.unshift(newTodo);
         form.reset();
         render()
+
     } else {
         const dNone = document.querySelector('.empty__btn')
         const inp = document.querySelector('input')
@@ -127,7 +181,7 @@ form.addEventListener("submit", (e) => {
             dNone.style.cssText = `display: none
             `
             inp.classList.remove('new__input')
-        }, 1000)
+        }, 400)
     }
 })
 
@@ -180,8 +234,10 @@ items.addEventListener("click", (e) => {
         saveButton.style.display = "block";
         cancelButton.style.display = "block";
         editButton.style.display = "none";
+        deleteButton.style.display = "none";
 
         currentInput.removeAttribute("disabled");
+
         currentInput.focus();
 
         const value = currentInput.value;
@@ -198,23 +254,53 @@ items.addEventListener("click", (e) => {
                 editButton.style.display = "block";
                 console.log('blur');
                 render()
-            }, 1000);
+            }, 500);
         });
     }
     if (e.target.closest(".save")) {
-        // e.stopPropagation()
-        console.log("save");
+
         saveButton.style.display = "none";
         cancelButton.style.display = "none";
         editButton.style.display = "block";
         currentInput.setAttribute("disabled", "");
 
-        todos = todos.map((e) =>
-            e.id === currentId ? { ...e, todo: currentInput.value } : e
-        );
+        // todos = todos.map((e) =>
+        //     e.id === currentId ? { ...e, todo: currentInput.value } : e
+        // );
+
+        for (let i = 0; i < todos.length; i++) {
+            if (todos[i].id === currentId) {
+                todos[i].todo = currentInput.value;
+            }
+        }
+
 
         render();
     }
+
+
+    currentInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            saveButton.style.display = "none";
+            cancelButton.style.display = "none";
+            editButton.style.display = "block";
+            currentInput.setAttribute("disabled", "");
+
+            // todos = todos.map((e) =>
+            //     e.id === currentId ? { ...e, todo: currentInput.value } : e
+            // );
+
+
+            for (let i = 0; i < todos.length; i++) {
+                if (todos[i].id === currentId) {
+                    todos[i].todo = currentInput.value;
+                }
+            }
+
+            render();
+        }
+    })
 
     if (e.target.closest(".cancel")) {
         saveButton.style.display = "none";
@@ -224,6 +310,16 @@ items.addEventListener("click", (e) => {
         render();
     }
 
+
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "Escape" && saveButton.style.display === "block") {
+            saveButton.style.display = "none";
+            cancelButton.style.display = "none";
+            editButton.style.display = "block";
+            currentInput.setAttribute("disabled", "");
+            render();
+        }
+    });
 
     if (e.target.closest(".checkbox")) {
         // todos = todos.map((e) =>
@@ -236,25 +332,62 @@ items.addEventListener("click", (e) => {
         render();
     }
 
-    document.addEventListener('keydown', (e) => {
-        if (e.code === 'Escape') {
-            render();
-        }
-        if (e.code === 'Enter') {
-            // saveButton.style.display = "none";
-            // cancelButton.style.display = "none";
-            // editButton.style.display = "block";
-            // currentInput.setAttribute("disabled", "");
-
-            // todos = todos.map((e) =>
-            //     e.id === currentId ? { ...e, todo: currentInput.value } : e
-            // );
-
-            // render();
-        }
-    })
 
 
 })
+
+
+
+// const wrapper = document.getElementById('wrapper');
+
+// var images = ["115.jpg", "116.jpg", "117.jpg"];
+
+// $(function () {
+//     var i = 0;
+//     wrapper.css("background-image", "url(images/" + images[i] + ")");
+//     setInterval(function () {
+//         i++;
+//         if (i == images.length) {
+//             i = 0;
+//         }
+//         wrapper.fadeOut("slow", function () {
+//             $(this).css("background-image", "url(images/" + images[i] + ")");
+//             $(this).fadeIn("slow");
+//         });
+//     }, 5000);
+// });
+
+const wrapper = document.getElementById("wrapper");
+const images = [
+    "1535120110.jpg",
+    "118.jpg", "120.jpg",
+    "121.jpg",
+    "127.jpg",
+    "128.jpg"
+];
+let i = 0;
+
+function changeImage() {
+    wrapper.style.transition = `2s`
+    wrapper.style.background = `url(images/${images[i]}) no-repeat center center fixed`;
+    wrapper.style.mozBackgroundSize = "cover";
+    wrapper.style.oBackgroundSize = "cover";
+    wrapper.style.backgroundSize = "cover";
+    i++;
+    if (i == images.length) {
+        i = 0;
+    }
+    setTimeout(changeImage, 10000);
+}
+
+changeImage();
+
+
+
+
+
+
+
+
 
 
